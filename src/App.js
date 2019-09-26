bd451c065e0e999a47dd655e2d79cd7d9c3bad5d
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-09-26 19:49:06 +0200

    Metamask first payment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 import Countdown from 'react-countdown-now'
 import Typography from '@material-ui/core/Typography';
 import getWeb3 from './utils/getWeb3';
+import contractAbi from "./contracts/abi";
+
+const CONTRACT_ADDRESS = '0x41B301c0b0AbbFEef99803c23A281712e29B6EF1'
 
 class App extends React.Component {
 
@@ -13,6 +16,10 @@ class App extends React.Component {
     this.freePass = this.freePass.bind(this)
     this.renderer = this.renderer.bind(this)
     this.pad = this.pad.bind(this)
+    this.ephemeralKeyPair = undefined
+    this.web3 = undefined
+    this.contract = undefined
+    this.ephemeral_key = undefined
     this.state = {
       connected: false,
       timeLeft: 0
@@ -20,8 +27,13 @@ class App extends React.Component {
   }
 
   async freePass() {
-    let response = await axios.post('/api/mac', { "timeLeft": 7200, "txId": null })
+    // let response = await axios.post('/api/mac', { "timeLeft": 7200, "txId": null })
+    const amount = '3000000000000000'
+    const receipt = await this.state.contract.methods.openChannel(this.state.ephemeral.address).send(
+      { from: this.state.accounts[0], value: amount, gas: '1000000' })
+    console.log(receipt)
     this.setState({ connected: true, timeLeft: 7200, start: Date.now() })
+    console.log(this.state.ephemeral)
   }
 
   pad(num, size) {
@@ -31,13 +43,22 @@ class App extends React.Component {
 
   async componentDidMount() {
     const web3 = await getWeb3()
-    let response = await axios.get('/api/mac')
+    const accounts = await web3.eth.getAccounts();
+    console.log(CONTRACT_ADDRESS)
+    const contract = new web3.eth.Contract(
+      contractAbi,
+      CONTRACT_ADDRESS
+    )
+    let response = await axios.get('api/mac')
+    const ephemeral = web3.eth.accounts.create()
+    console.log(ephemeral)
     if (response.status === 204) {
-      this.setState({ connected: false, timeLeft: 0 })
+      this.setState({ connected: false, timeLeft: 0, contract: contract, web3: web3, accounts: accounts, ephemeral: ephemeral })
     } else {
       let now = Date.now()
       let timeLeft = response.data.timeLeft - (now - response.data.now) / 1000
-      this.setState({ connected: true, start: now, timeLeft: timeLeft })
+      this.setState({ connected: true, start: now, timeLeft: timeLeft, contract: contract,
+                      web3: web3, accounts: accounts, ephemeral: ephemeral })
     }
   }
 
