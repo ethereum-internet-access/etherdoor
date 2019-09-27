50f17f76ba084858228a5baafe6dd9d30ac51488
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-09-27 14:23:59 +0200

    Component refactor, add loading indicator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './App.css';
+
 import Pricing from './containers/Pricing.js'
 import axios from 'axios';
 import Countdown from 'react-countdown-now'
-import Typography from '@material-ui/core/Typography';
 import getWeb3 from './utils/getWeb3';
 import contractAbi from "./contracts/abi";
+import { trackPromise } from 'react-promise-tracker'
+
+import './App.css';
+import LoadingIndicator from './components/LoadingIndicator'
 
 const CONTRACT_ADDRESS = '0x41B301c0b0AbbFEef99803c23A281712e29B6EF1'
 
@@ -15,7 +18,6 @@ class App extends React.Component {
     super(props)
     this.freePass = this.freePass.bind(this)
     this.renderer = this.renderer.bind(this)
-    this.pad = this.pad.bind(this)
     this.ephemeralKeyPair = undefined
     this.web3 = undefined
     this.contract = undefined
@@ -27,18 +29,13 @@ class App extends React.Component {
   }
 
   async freePass() {
-    // let response = await axios.post('/api/mac', { "timeLeft": 7200, "txId": null })
     const amount = '3000000000000000'
-    const receipt = await this.state.contract.methods.openChannel(this.state.ephemeral.address).send(
-      { from: this.state.accounts[0], value: amount, gas: '1000000' })
-    console.log(receipt)
-    this.setState({ connected: true, timeLeft: 7200, start: Date.now() })
-    console.log(this.state.ephemeral)
-  }
-
-  pad(num, size) {
-    var s = '000000000' + num;
-    return s.substr(s.length-size);
+    // await axios.post('/api/mac', { "timeLeft": 7200, "txId": null })
+    trackPromise(
+      this.state.contract.methods.openChannel(this.state.ephemeral.address).send(
+        { from: this.state.accounts[0], value: amount, gas: '1000000' }).then(
+          this.setState({ connected: true, timeLeft: 7200, start: Date.now() })).catch(
+            () => this.setState({ connected: false, timeLeft: 0, start: Date.now() })))
   }
 
   async componentDidMount() {
@@ -51,7 +48,6 @@ class App extends React.Component {
     )
     let response = await axios.get('api/mac')
     const ephemeral = web3.eth.accounts.create()
-    console.log(ephemeral)
     if (response.status === 204) {
       this.setState({ connected: false, timeLeft: 0, contract: contract, web3: web3, accounts: accounts, ephemeral: ephemeral })
     } else {
@@ -71,20 +67,7 @@ class App extends React.Component {
       )
     } else {
       return (
-          <div className="App">
-          <br/>
-          <Typography variant="h6" color="inherit" noWrap align="center">
-          Internet Access Token
-          </Typography>
-          <br/>
-          <Typography variant="h4" align="center">
-          Remaining connection time ...
-          </Typography>
-          <br/>
-          <Typography variant="h2" color="textSecondary" align="center">
-          { this.pad(hours, 2)}:{this.pad(minutes, 2)}:{this.pad(seconds, 2) }
-          </Typography>
-          </div>
+          <LoadingIndicator hours={hours} minutes={minutes} seconds={seconds}/>
       )
     }
   }
